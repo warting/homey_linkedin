268501cbf127307d214530cc158dd29461f8df42
Stefan Wärting <stefan@warting.se>
2025-05-18 13:58:24 +0200

    Enhance OAuth2Driver class with additional lifecycle methods and configuration management, including onInit, onUninit, and methods for pairing and device management.

--- types/OAuth2Driver.d.ts
+++ types/OAuth2Driver.d.ts
@@ -1,11 +1,62 @@
 import Homey from 'homey';
-import { OAuth2Client } from './OAuth2Client';
 
+/**
+ * OAuth2Driver
+ *
+ * @extends Homey.Driver
+ */
 export class OAuth2Driver extends Homey.Driver {
-  getOAuth2Client<T extends OAuth2Client>(): T;
-  setStoreValue(key: string, value: any): Promise<void>;
-  getStoreValue(key: string): Promise<any>;
-  onOAuth2Init?(): Promise<void>;
+  /**
+   * @returns {*}
+   */
+  getOAuth2ConfigId(): any;
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async onInit(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Init(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Uninit(): Promise<void>;
+
+  /**
+   * @param {PairSession} socket
+   */
+  onPair(socket: any): void;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<*>}
+   */
+  async onPairListDevices(): Promise<any>;
+
+  /**
+   * @param {PairSession} socket
+   * @param {Homey.Device} device
+   */
+  onRepair(socket: any, device: Homey.Device): void;
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async onUninit(): Promise<void>;
+
+  /**
+   * @param {string} id
+   */
+  setOAuth2ConfigId(id: string): void;
 }
 
 export default OAuth2Driver;
