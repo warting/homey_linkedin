066ab20f0e9f43d43b87dda194989ef6b1f82330
Stefan Wärting <stefan@warting.se>
2025-05-18 13:56:22 +0200

    Enhance OAuth2Device class with additional lifecycle methods for improved extensibility, including onInit, onUninit, and various OAuth2 event handlers.

--- types/OAuth2Device.d.ts
+++ types/OAuth2Device.d.ts
@@ -1,14 +1,79 @@
 import Homey from 'homey';
-import { OAuth2Client } from './OAuth2Client';
 
+/**
+ * OAuth2Device
+ *
+ * @extends Homey.Device
+ */
 export class OAuth2Device extends Homey.Device {
-  // The OAuth2Client instance for this device
-  oAuth2Client: OAuth2Client;
+  /**
+   * @returns {Promise<void>}
+   */
+  async onAdded(): Promise<void>;
 
-  // Methods
-  onOAuth2Init(): Promise<void>;
-  onOAuth2Added?(): Promise<void>;
-  onOAuth2Deleted?(): Promise<void>;
+  /**
+   * @returns {Promise<void>}
+   */
+  async onDeleted(): Promise<void>;
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async onInit();
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Added(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Deleted(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Destroyed(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Expired(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Init(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Saved(): Promise<void>;
+
+  /**
+   * This method can be extended
+   *
+   * @returns {Promise<void>}
+   */
+  async onOAuth2Uninit(): Promise<void>;
+
+  /**
+   * @returns {Promise<void>}
+   */
+  async onUninit(): Promise<void>;
 }
 
 export default OAuth2Device;
